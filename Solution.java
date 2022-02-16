782f11677351453dc8b66b38b97c4d5fc6ce2b06
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-02-16 20:09:59 +0100

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public int[] pivotArray(int[] nums, int pivot) {
+
+        List<Integer> lessThanPivot = new ArrayList<>();
+        List<Integer> greaterThanPivot = new ArrayList<>();
+
+        for (int n : nums) {
+            if (n < pivot) {
+                lessThanPivot.add(n);
+            } else if (n > pivot) {
+                greaterThanPivot.add(n);
+            }
+        }
+
+        int index = 0;
+        int equalToPivot = nums.length - (lessThanPivot.size() + greaterThanPivot.size());
+
+        for (int n : lessThanPivot) {
+            nums[index++] = n;
+        }
+
+        while (equalToPivot-- > 0) {
+            nums[index++] = pivot;
+        }
+
+        for (int n : greaterThanPivot) {
+            nums[index++] = n;
+        }
+
+        return nums;
+    }
+}
